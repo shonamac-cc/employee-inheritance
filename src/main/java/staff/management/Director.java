967e90a283a360d33829e0e9dd17f595b1f9cbeb
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-10-14 16:31:33 +0100

    added director tests

--- src/main/java/staff/management/Director.java
+++ src/main/java/staff/management/Director.java
@@ -1,4 +1,15 @@
 package staff.management;
 
-public class Director {
+public class Director extends Manager{
+
+    private double budget;
+
+    public Director(String name, String niNumber, double salary, String deptName, double budget){
+        super(name, niNumber, salary, deptName);
+        this.budget = budget;
+    }
+
+    public double getBudget(){
+        return this.budget;
+    }
 }
---
+++ src/test/java/DirectorTest.java
@@ -0,0 +1,32 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.management.Director;
+
+import static org.junit.Assert.assertEquals;
+
+public class DirectorTest {
+
+    Director director;
+
+    @Before
+    public void before() {
+        director = new Director("Corabeth Godsey", "DD987654V", 90000, "IT", 65000);
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        director.raiseSalary(10000);
+        assertEquals(100000, director.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canPayBonus(){
+        assertEquals(900, director.payBonus(), 0.01);
+    }
+
+    @Test
+    public void canGetBudget(){
+        assertEquals(65000, director.getBudget(), 0.01);
+    }
+
+}
